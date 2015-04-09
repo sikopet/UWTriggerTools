782728dd8a405bcd0bed48001756535243409d64
Aaron Levine <aglevine@wisc.edu>
2015-04-09 13:37:39 -0500

    updated for strucutre of products in 7_4_X (isolated taus produced seperately)

--- test/NtupilizeEmulatorEffi.py
+++ test/NtupilizeEmulatorEffi.py
@@ -22,14 +22,14 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 #Max Events
 process.maxEvents = cms.untracked.PSet(
-	#input = cms.untracked.int32(options.maxEvents)
-        input = cms.untracked.int32(3)
+	input = cms.untracked.int32(options.maxEvents)
+	#input = cms.untracked.int32(100)
 )
 
 #Input
 process.source = cms.Source("PoolSource",
 	fileNames = cms.untracked.vstring(options.inputFiles)
-#        fileNames = cms.untracked.vstring("/store/mc/Fall13dr/Neutrino_Pt-2to20_gun/GEN-SIM-RAW/tsg_PU40bx25_POSTLS162_V2-v1/00005/C2D9064D-FB7F-E311-BFCC-003048679228.root")
+        #fileNames = cms.untracked.vstring("/store/mc/Fall13dr/Neutrino_Pt-2to20_gun/GEN-SIM-RAW/tsg_PU40bx25_POSTLS162_V2-v1/00005/C2D9064D-FB7F-E311-BFCC-003048679228.root")
 )
 
 #Output
@@ -40,10 +40,24 @@ process.TFileService = cms.Service(
 
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag.connect = cms.string('frontier://FrontierProd/CMS_COND_31X_GLOBALTAG')
-process.GlobalTag.globaltag = cms.string('POSTLS162_V2::All')
+process.GlobalTag.globaltag = cms.string('POSTLS172_V2::All')
 
 process.load('L1Trigger.L1TCalorimeter.L1TCaloStage1_PPFromRaw_cff')
 process.load("L1Trigger.UWTriggerTools.recoObjects_cfi")
+
+from CondCore.DBCommon.CondDBSetup_cfi import CondDBSetup
+process.newRCTConfig = cms.ESSource("PoolDBESSource",
+    CondDBSetup,
+    connect = cms.string('frontier://FrontierPrep/CMS_COND_L1T'),
+    DumpStat=cms.untracked.bool(True),
+    toGet = cms.VPSet(
+        cms.PSet(
+            record = cms.string('L1RCTParametersRcd'),
+            tag = cms.string('L1RCTParametersRcd_L1TDevelCollisions_ExtendedScaleFactorsV2')
+        ) 
+    ) 
+)
+process.prefer("newRCTConfig")
 
 reco_object_step = process.recoObjects
 process.p1 = cms.Path(
@@ -56,28 +70,46 @@ process.TauEmulEffi = cms.EDAnalyzer(
 	"MiniEffiTree",
         src = cms.VInputTag("recoTaus"),
         #src = cms.VInputTag("isoTaus"),
-        genSrc = cms.VInputTag(cms.InputTag("l1ExtraLayer2","Tau")),
-        #genSrc = cms.VInputTag(cms.InputTag("caloStage1Digis","rlxTaus")),
+        L1Src = cms.VInputTag(cms.InputTag("l1ExtraLayer2","Tau")),
+        #L1Src = cms.VInputTag(cms.InputTag("caloStage1Digis","rlxTaus")),
         srcJet = cms.VInputTag("ak5PFJets"),
-        genSrcJet = cms.VInputTag(cms.InputTag("l1ExtraLayer2","Central")),
+        L1SrcJet = cms.VInputTag(cms.InputTag("l1ExtraLayer2","Central")),
         srcEg = cms.VInputTag("recoElecs"),
-        genSrcEg = cms.VInputTag(cms.InputTag("l1ExtraLayer2","NonIsolated")),
+        L1SrcEg = cms.VInputTag(cms.InputTag("l1ExtraLayer2","NonIsolated")),
         metSrc = cms.VInputTag("tcMet"),
-	genMetSrc = cms.VInputTag(cms.InputTag("l1ExtraLayer2","MET"))
+	L1MetSrc = cms.VInputTag(cms.InputTag("l1ExtraLayer2","MET"))
 )
+process.TauEmulEffiIso =  cms.EDAnalyzer(
+        "MiniEffiTree",
+        src = cms.VInputTag("recoTaus"),
+        #src = cms.VInputTag("isoTaus"),
+        L1Src = cms.VInputTag(cms.InputTag("l1ExtraLayer2","IsoTau")),
+        #L1Src = cms.VInputTag(cms.InputTag("caloStage1Digis","rlxTaus")),
+        srcJet = cms.VInputTag("ak5PFJets"),
+        L1SrcJet = cms.VInputTag(cms.InputTag("l1ExtraLayer2","Central")),
+        srcEg = cms.VInputTag("recoElecs"),
+        L1SrcEg = cms.VInputTag(cms.InputTag("l1ExtraLayer2","NonIsolated")),
+        metSrc = cms.VInputTag("tcMet"),
+        L1MetSrc = cms.VInputTag(cms.InputTag("l1ExtraLayer2","MET"))
+)
+
+process.l1RCTParametersTest = cms.EDAnalyzer("L1RCTParametersTester")
+
 
 #process.EGEmulEffi = cms.EDAnalyzer(
 #	"MiniEffiTree",
 #        src = cms.VInputTag("recoElecs"),
-#        genSrc = cms.VInputTag(cms.InputTag("l1ExtraLayer2","NonIsolated")),
+#        L1Src = cms.VInputTag(cms.InputTag("l1ExtraLayer2","NonIsolated")),
 #        srcJet = cms.VInputTag("ak5PFJets"),
-#        genSrcJet = cms.VInputTag(cms.InputTag("l1ExtraLayer2","Central")),
+#        L1SrcJet = cms.VInputTag(cms.InputTag("l1ExtraLayer2","Central")),
 #        metSrc = cms.VInputTag("tcMet"),
-#        genMetSrc = cms.VInputTag(cms.InputTag("l1ExtraLayer2","MET"))
+#        L1MetSrc = cms.VInputTag(cms.InputTag("l1ExtraLayer2","MET"))
 #)
 
 process.p2 = cms.Path(
 	process.TauEmulEffi
+	+process.TauEmulEffiIso
+	+process.l1RCTParametersTest
  	#+ process.EGEmulEffi
 )
 
